8924ae9d98a5cd4d442477e2c6a0cbbf898d244a
firoliyos <firatesfaye44@gmail.com>
2025-02-19 15:02:14 +0300

    content-displayed

--- firflix/src/App.js
+++ firflix/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import requests from './requests';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Row 
+        title='Netflix Originals'
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row 
+        title='Trending Now'
+        fetchUrl={requests.fetchTrending}
+       />
     </div>
   );
 }
