dff2d1157a3b7f4d5f9b4ab46f541cd3bbc8e22c
firoliyos <firatesfaye44@gmail.com>
2025-02-26 15:03:41 +0300

    play-functionality-on-Banner.js(Plyay-button

--- firflix/src/components/Banner/Banner.js
+++ firflix/src/components/Banner/Banner.js
@@ -1,50 +1,73 @@
-import React, { useEffect, useState } from 'react'
-import axios from '../../utils/axios'
-import requests from '../../utils/requests'
-import './Banner.css'
+import React, { useEffect, useState } from 'react';
+import axios from '../../axios';
+import requests from '../../requests';
+import './Banner.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 function Banner() {
-    const [movie, setMovie] = useState([])
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(requests.fetchNetflixOriginals)
-            setMovie(
-                request?.data.results[
-                    Math.floor(Math.random() * request.data.results.length)])
-            return request;
-        }
-        fetchData()
-    }, [])
-    //  console.log(movie)
-    function truncate(str, n) {
-        return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
     }
+    fetchData();
+  }, []);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || '')
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
     <header
-        className='banner'
-        style={{
-            backgroundSize: 'cover',
-            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
-            backgroundPosition: 'center center',
-        }}
-     >
-        <div className='banner_contents'>
-            <h1 className='banner_title'>
-                {movie?.title || movie?.name || movie?.original_name}
-            </h1>
-            <div className='banner_buttons'>
-                <button className='banner_button'>
-                    Play
-                </button>
-                <button className='banner_button'>
-                    My List
-                 </button>
-            </div>
-            <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
+      className="banner"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: 'center center',
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button" onClick={() => handleClick(movie)}>
+            Play
+          </button>
+          <button className="banner_button">My List</button>
         </div>
-        <div className='banner_fadeBottom'></div>
-    </header> 
-  )
+        <h1 className="banner_description">{movie?.overview}</h1>
+      </div>
+      <div className="banner--fadeBottom" />
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
