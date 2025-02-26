f8d58c2eb07894ac458e3fb90c6bfe64e37c8eed
firoliyos <firatesfaye44@gmail.com>
2025-02-26 14:27:23 +0300

    Row-done(.js and .cc)

--- firflix/src/Row.js
+++ firflix/src/Row.js
@@ -1,67 +1,62 @@
-import React, { useEffect, useState } from 'react'
-import './Row.css'
-import axios from './axios'
+import React, { useEffect, useState } from 'react';
+import './Row.css';
+import axios from './axios';
 import YouTube from 'react-youtube';
 import movieTrailer from 'movie-trailer';
+
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
-function Row({title, fetchUrl, isLargeRow}) {
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState('');
 
-   const [movies, setMovies] = useState([])
-
-    const [trailerUrl, setTrailerUrl] = useState('')
-
-   useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
-        const request = await axios.get(fetchUrl);
-        //console.log(request.data.results);
-        setMovies(request.data.results)
-        return request;
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
     }
     fetchData();
-   }, [fetchUrl])
+  }, [fetchUrl]);
 
-    const opts = {
-        height: '390',
-        width: '100%',
-        playerVars: {
-            autoplay: 1,
-        },
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || '')
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+        })
+        .catch((error) => console.log(error));
     }
+  };
 
-   const handleClick = (movie) => {
-        if (trailerUrl) {
-            setTrailerUrl('')
-        } else {
-            movieTrailer(movie?.title || movie?.name || movie.original_name)
-              .then((url) => {
-                const urlParams = new URLSearchParams(new URL(url).search)
-                setTrailerUrl(urlParams.get('v'))
-            })
-            .catch((error) => console.log(error))
-        }
-   }
-
-   console.log(movies)
   return (
-    <div className='row'>
-      <h1>{title}</h1>
-      <div className='row_posters'>
-        {movies.map((movie) => (
-          <img 
-            onClick={() => handleClick(movie)}
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies && movies.map((movie) => (
+          <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
             src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-            alt={movie.name} 
+            alt={movie.name}
           />
         ))}
       </div>
-      <div style={{ padding: '40px' }}>
-        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
-  )
+  );
 }
 
-export default Row 
+export default Row;
