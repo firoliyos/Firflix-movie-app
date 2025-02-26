60c74d35e5cb0d67955aca22f346342c40c889bc
firoliyos <firatesfaye44@gmail.com>
2025-02-26 14:28:48 +0300

    Latest.js-done

---
+++ firflix/src/pages/Latest.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import axios from '../axios';
+import requests from '../requests';
+import Row from '../Row';
+
+function Latest() {
+  const [latest, setLatest] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+      setLatest(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="latest">
+      <h2>Latest</h2>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+    </div>
+  );
+}
+
+export default Latest;
